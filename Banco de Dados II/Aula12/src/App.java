e40236cec1dc3a1100c37a2480f722dbb974ef15
Eduardo Bonatto <edubonatto10@gmail.com>
2022-10-24 22:44:24 -0300

    Criando projeto da aula 11

---
+++ Banco de Dados II/Aula12/src/App.java
@@ -0,0 +1,24 @@
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class App {
+    public static void main(String args[]) {
+        Connection conn = null;
+
+        try {
+            conn = DriverManager.getConnection(
+                "jdbc:mysql://localhost:3306/teste",
+                "root",
+                ""
+            );
+
+            System.out.println("Conectado");
+            
+        } catch (SQLException e) {
+            System.out.println("ERRO: " + e.getMessage());
+            
+        }
+    }
+
+}
