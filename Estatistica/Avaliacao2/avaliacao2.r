129bd70a643537d51da0a1644a01c01c9bc96c0a
Eduardo Bonatto <edubonatto10@gmail.com>
2022-12-01 21:06:34 -0300

    Terminando avaliação 2

--- Estatistica/Avaliacao2/avaliacao2.r
+++ Estatistica/Avaliacao2/avaliacao2.r
@@ -1,4 +1,6 @@
 
+## Questão 1
+
 # Horas de estudos semanais
 h <- c(2, 7, 2, 1, 1, 1, 15)
 # Notas semestrais
@@ -11,7 +13,6 @@ coeficiente
 # [1] 0.9747042
 
 library(ggplot2)
-
 retas <- ggplot(mapping = aes(h, n)) +
          geom_point() +
          geom_smooth(se = FALSE, method = "lm")
@@ -28,3 +29,65 @@ sqt <- sum((n - mean(n)) ^ 2)
 r2 <- sqr / sqt
 r2
 # [1] 0.9500482
+
+## Questão 2
+
+options(digits = 4)
+
+## 90%
+n <- 650
+x <- 240
+p <- x / n
+alpha <- 0.1
+
+z_alpha1 <- qnorm(alpha / 2, mean = 0, sd = 1, lower.tail = FALSE)
+ic <- c(
+    p - z_alpha1 * sqrt(p * (1 - p) / n),
+    p + z_alpha1 * sqrt(p * (1 - p) / n)
+)
+ic # [1] 0.3381 0.4004
+
+## 95%
+n <- 650
+x <- 240
+p <- x / n
+alpha <- 0.05
+
+z_alpha1 <- qnorm(alpha / 2, mean = 0, sd = 1, lower.tail = FALSE)
+ic <- c(
+    p - z_alpha1 * sqrt(p * (1 - p) / n),
+    p + z_alpha1 * sqrt(p * (1 - p) / n)
+)
+ic # [1] 0.3321 0.4063
+
+## Questão 3
+
+## 90%
+options(digits = 8)
+
+x_med <- 450
+sigma <- 7
+alpha <- 0.05
+n <- 90
+
+z_alpha5 <- qnorm(alpha / 2, mean = 0, sd = 1, lower.tail = FALSE)
+ic <- c(
+    x_med - z_alpha5 * sigma / sqrt(n),
+    x_med + z_alpha5 * sigma / sqrt(n)
+)
+ic # [1] 448.55381 451.44619
+
+## 95%
+options(digits = 8)
+
+x_med <- 450
+sigma <- 7
+alpha <- 0.1
+n <- 90
+
+z_alpha5 <- qnorm(alpha / 2, mean = 0, sd = 1, lower.tail = FALSE)
+ic <- c(
+    x_med - z_alpha5 * sigma / sqrt(n),
+    x_med + z_alpha5 * sigma / sqrt(n)
+)
+ic # [1] 448.78632 451.21368
